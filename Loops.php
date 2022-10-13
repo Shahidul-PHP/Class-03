12f8209a271ec49bf1eea685c935d7929f06215e
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-13 08:50:11 +0600

    loops done

--- Loops.php
+++ Loops.php
@@ -1,60 +1,38 @@
 <?php 
 
-echo '<title>Loops in PHP</title>'
+
+// for loop
+for($i = 7; $i<= 86; $i+=6){
+    echo $i;
+
+    
+}
+
+echo '</br>';
+
+// while loop
+
+$x = 3;
+
+while($x <= 18){
+    echo $x;
+    $x+=4;
+}
+
+echo '</br>';
+
+// do while loop
 
 
+$i = 2;
+
+do{
+    echo $i;
+    $i+=5;
+}
+while($i >= 20)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for each loop use in array and object //
 
 ?>
