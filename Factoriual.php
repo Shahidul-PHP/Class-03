5be2422eca6ff72bda07ce4cb692e30b658d0ad1
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-13 10:45:05 +0600

    factorial done

--- Factoriual.php
+++ Factoriual.php
@@ -1,49 +1,22 @@
 <?php 
 
-echo '<title>Factorial in PHP</title>'
+echo '<title>Factorial in PHP</title>';
+
+echo '<h2>Find Factorial Using PHP</h2>';
 
 
+$FactorialNeed = 50;
+$end = 1;
+
+for($x = $FactorialNeed; $x >= 1; $x--){
+    $end = $end * $x;
+    echo "Big to small number = " .$x . '<br>';
+}
+
+echo '<br>';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "Factoirial of ". $FactorialNeed." is = ".$end;
 
 
 ?>
