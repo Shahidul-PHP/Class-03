d4be7effc4b172b4a770c94d9b94151694da4e33
Shahidul-PHP <Shahidul.shovon14@gmail.com>
2022-10-14 11:10:31 +0600

    done

--- HomeTaskthree.php
+++ HomeTaskthree.php
@@ -19,13 +19,24 @@ for ($i = 0; $i < 5; $i++) {
 
 echo '<br>';
 
-for ($i = 1; $i <= 15; $i++) {
+for ($i = 1; $i < 10; $i++) {
     for ($j = 1; $j <= $i; $j++) {
-        echo '*';
+        echo "* ";
     }
     echo '<br>';
 }
 
+for ($i = 1; $i <= 5; $i++) {
+    for ($j = 4; $j >= $i; $j--)  //loop to print spaces
+    {
+        echo '&nbsp;&nbsp;';
+    }
+    for ($k = 1; $k <= $i; $k++)  //loop to print stars
+    {
+        echo '*';
+    }
+    echo '<br>';
+}
 
 
 
--- Loops.php
+++ Loops.php
@@ -1,30 +1,65 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-for($i = 1; $i <= 15; $i++){
-    
-}
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .text1 {
+            color: orange;
+            font-size: 2rem;
+        }
+
+        .text2 {
+            color: red;
+            font-size: 2rem;
+        }
+
+        .text3 {
+            color: green;
+            font-size: 2rem;
+        }
+    </style>
+</head>
+
+<body>
+
+    <?php
+    // $text = 'W3resource';
+
+    // echo $text.'<br>';
+
+    // echo trim($text,"Wres");
 
 
 
+    // function fname($test){
+    //     for($i=2; $i <= 40; $i+=3){
+    //         echo $i . "loop and function test". "<br>";
 
+    //         if($i == 30){
+    //             echo $i."stop the loop here !!";
+    //          break;   
+    //         }
+    //     }
+    // }
 
+    // fname('ajmot');
 
+    $n = 168956;
+    $count = strlen($n);
 
+    if ($count == 8) {
+        echo "<p class='text3'>Your Password is Correct</p>";
+    } elseif ($count < 8) {
+        echo "<p class='text2'>Password should at least 8 Character !!</p>";
+    } elseif ($count > 8) {
+        echo "<p class='text1'>Your Password is Cross the Limit . Password should at least 8 Character</p>";
+    }
 
+    ?>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
+</body>
+</html>
